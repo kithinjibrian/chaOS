c2026098efdc522d5e0985c72d3db6c4388d8797
kithinjibrian <kithinjibrian369@gmail.com>
2024-07-22 18:09:37 +0300

    Grab IDT

---
+++ src/cpu/include/idt.h
@@ -0,0 +1,95 @@
+#ifndef IDT_H
+#define IDT_H
+
+#include "../../libc/include/type.h"
+#include "../../drivers/include/vga.h"
+#include "../../libc/include/mstring.h"
+#include "../../drivers/include/port.h"
+
+// remove padding from structs between the pragma directives
+#pragma pack(push, 1)
+
+typedef struct idt_entry_t
+{
+	u16_t base_lo;
+	u16_t sel;
+	u8_t always0;
+	u8_t flags;
+	u16_t base_hi;
+} idt_entry_t;
+
+typedef struct idt_ptr_t
+{
+	u16_t limit;
+	u32_t base;
+} idt_ptr_t;
+
+#pragma pack(pop)
+
+typedef struct registers_t
+{
+	u32_t ds;
+	u32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+	u32_t int_no, err_code;
+	u32_t eip, cs, eflags, user_esp, user_ss;
+} registers_t;
+
+typedef void (*fun_handler_t)(registers_t);
+
+extern void idt_flush(u32_t);
+
+extern void isr0();
+extern void isr1();
+extern void isr2();
+extern void isr3();
+extern void isr4();
+extern void isr5();
+extern void isr6();
+extern void isr7();
+extern void isr8();
+extern void isr9();
+extern void isr10();
+extern void isr11();
+extern void isr12();
+extern void isr13();
+extern void isr14();
+extern void isr15();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+extern void isr20();
+extern void isr21();
+extern void isr22();
+extern void isr23();
+extern void isr24();
+extern void isr25();
+extern void isr26();
+extern void isr27();
+extern void isr28();
+extern void isr29();
+extern void isr30();
+extern void isr31();
+
+extern void irq0();
+extern void irq1();
+extern void irq2();
+extern void irq3();
+extern void irq4();
+extern void irq5();
+extern void irq6();
+extern void irq7();
+extern void irq8();
+extern void irq9();
+extern void irq10();
+extern void irq11();
+extern void irq12();
+extern void irq13();
+extern void irq14();
+extern void irq15();
+
+void init_idt(void);
+void isr_handler(registers_t regs);
+void irq_handler(registers_t regs);
+
+#endif
---
+++ src/cpu/src/idt.c
@@ -0,0 +1,166 @@
+#include "idt.h"
+
+idt_entry_t idt[256];
+idt_ptr_t idt_ptr;
+
+static void set_idt_gate(int num, u32_t base, u16_t sel, u8_t flags)
+{
+	idt[num].base_lo = base & 0xFFFF;
+	idt[num].base_hi = (base >> 16) & 0xFFFF;
+	idt[num].sel = sel;
+	idt[num].always0 = 0;
+	idt[num].flags = flags | 0x60;
+}
+
+void init_idt(void)
+{
+	idt_ptr.limit = (sizeof(idt_entry_t) * 256) - 1;
+	idt_ptr.base = (u32_t)&idt;
+
+	memset(&idt, 0, sizeof(idt_entry_t) * 256);
+
+	port_byte_out(0x20, 0x11);
+	port_byte_out(0xA0, 0x11);
+
+	port_byte_out(0x21, 0x20);
+	port_byte_out(0xA1, 0x28);
+
+	port_byte_out(0x21, 0x04);
+	port_byte_out(0xA1, 0x02);
+
+	port_byte_out(0x21, 0x01);
+	port_byte_out(0xA1, 0x01);
+
+	port_byte_out(0x21, 0x0);
+	port_byte_out(0xA1, 0x0);
+
+	set_idt_gate(0, (u32_t)isr0, 0x08, 0x8E);
+	set_idt_gate(1, (u32_t)isr1, 0x08, 0x8E);
+	set_idt_gate(2, (u32_t)isr2, 0x08, 0x8E);
+	set_idt_gate(3, (u32_t)isr3, 0x08, 0x8E);
+	set_idt_gate(4, (u32_t)isr4, 0x08, 0x8E);
+	set_idt_gate(5, (u32_t)isr5, 0x08, 0x8E);
+	set_idt_gate(6, (u32_t)isr6, 0x08, 0x8E);
+	set_idt_gate(7, (u32_t)isr7, 0x08, 0x8E);
+	set_idt_gate(8, (u32_t)isr8, 0x08, 0x8E);
+	set_idt_gate(9, (u32_t)isr9, 0x08, 0x8E);
+	set_idt_gate(10, (u32_t)isr10, 0x08, 0x8E);
+	set_idt_gate(11, (u32_t)isr11, 0x08, 0x8E);
+	set_idt_gate(12, (u32_t)isr12, 0x08, 0x8E);
+	set_idt_gate(13, (u32_t)isr13, 0x08, 0x8E);
+	set_idt_gate(14, (u32_t)isr14, 0x08, 0x8E);
+	set_idt_gate(15, (u32_t)isr15, 0x08, 0x8E);
+	set_idt_gate(16, (u32_t)isr16, 0x08, 0x8E);
+	set_idt_gate(17, (u32_t)isr17, 0x08, 0x8E);
+	set_idt_gate(18, (u32_t)isr18, 0x08, 0x8E);
+	set_idt_gate(19, (u32_t)isr19, 0x08, 0x8E);
+	set_idt_gate(20, (u32_t)isr20, 0x08, 0x8E);
+	set_idt_gate(21, (u32_t)isr21, 0x08, 0x8E);
+	set_idt_gate(22, (u32_t)isr22, 0x08, 0x8E);
+	set_idt_gate(23, (u32_t)isr23, 0x08, 0x8E);
+	set_idt_gate(24, (u32_t)isr24, 0x08, 0x8E);
+	set_idt_gate(25, (u32_t)isr25, 0x08, 0x8E);
+	set_idt_gate(26, (u32_t)isr26, 0x08, 0x8E);
+	set_idt_gate(27, (u32_t)isr27, 0x08, 0x8E);
+	set_idt_gate(28, (u32_t)isr28, 0x08, 0x8E);
+	set_idt_gate(29, (u32_t)isr29, 0x08, 0x8E);
+	set_idt_gate(30, (u32_t)isr30, 0x08, 0x8E);
+	set_idt_gate(31, (u32_t)isr31, 0x08, 0x8E);
+
+	set_idt_gate(32, (u32_t)irq0, 0x08, 0x8E);
+	set_idt_gate(33, (u32_t)irq1, 0x08, 0x8E);
+	set_idt_gate(34, (u32_t)irq2, 0x08, 0x8E);
+	set_idt_gate(35, (u32_t)irq3, 0x08, 0x8E);
+	set_idt_gate(36, (u32_t)irq4, 0x08, 0x8E);
+	set_idt_gate(37, (u32_t)irq5, 0x08, 0x8E);
+	set_idt_gate(38, (u32_t)irq6, 0x08, 0x8E);
+	set_idt_gate(39, (u32_t)irq7, 0x08, 0x8E);
+	set_idt_gate(40, (u32_t)irq8, 0x08, 0x8E);
+	set_idt_gate(41, (u32_t)irq9, 0x08, 0x8E);
+	set_idt_gate(42, (u32_t)irq10, 0x08, 0x8E);
+	set_idt_gate(43, (u32_t)irq11, 0x08, 0x8E);
+	set_idt_gate(44, (u32_t)irq12, 0x08, 0x8E);
+	set_idt_gate(45, (u32_t)irq13, 0x08, 0x8E);
+	set_idt_gate(46, (u32_t)irq14, 0x08, 0x8E);
+	set_idt_gate(47, (u32_t)irq15, 0x08, 0x8E);
+
+	idt_flush((u32_t)&idt_ptr);
+}
+
+unsigned char *exception_messages[] = {
+	"Division By Zero",
+	"Debug",
+	"Non Maskable Interrupt",
+	"Breakpoint",
+	"Overflow",
+	"Bound Range Exceeded",
+	"Invalid Opcode",
+	"Device Not Available",
+
+	"Double Fault",
+	"Coprocessor Segment Overrun",
+	"Invalid TSS",
+	"Segment Not Present",
+	"Stack Fault",
+	"General Protection Fault",
+	"Page Fault",
+	"Unknown Interrupt",
+
+	"Coprocessor Fault",
+	"Alignment Check",
+	"Machine Check",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+	"Reserved",
+};
+
+void isr_handler(registers_t regs)
+{
+	if (regs.int_no < 32)
+	{
+		print(exception_messages[regs.int_no]);
+		while (1)
+			;
+	}
+}
+
+void *irqs[16] = {
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0};
+
+void irq_reg_handler(int irq, fun_handler_t handler)
+{
+	irqs[irq] = handler;
+}
+
+void irq_unreg_handler(int irq)
+{
+	irqs[irq] = 0;
+}
+
+void irq_handler(registers_t regs)
+{
+	fun_handler_t handler = irqs[regs.int_no];
+	if (handler)
+	{
+		handler(regs);
+	}
+
+	if (regs.int_no >= 40)
+	{
+		port_byte_out(0xA0, 0x20);
+	}
+
+	port_byte_out(0x20, 0x20);
+}
