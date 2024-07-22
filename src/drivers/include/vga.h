5e40f6e27180a2585d72153f04a5823500fb0919
kithinjibrian <kithinjibrian369@gmail.com>
2024-07-22 12:27:50 +0300

    clear VGA

--- src/drivers/include/vga.h
+++ src/drivers/include/vga.h
@@ -1,10 +1,17 @@
 #ifndef VGA_H
 #define VGA_H
 
+#include "port.h"
+
 #define MAX_COLS 80
 #define MAX_ROWS 25
 #define VIDEO_ADDRESS 0xB8000
 
+/* Screen i/o ports */
+#define REG_SCREEN_CTRL 0x3d4
+#define REG_SCREEN_DATA 0x3d5
+
+void clear();
 void print();
 
 #endif
--- src/drivers/src/vga.c
+++ src/drivers/src/vga.c
@@ -15,6 +15,24 @@ static int get_offset_col(int offset)
 	return (offset - (get_offset_row(offset) * 2 * MAX_COLS)) / 2;
 }
 
+static void set_cursor_offset(int offset)
+{
+	offset /= 2;
+	port_byte_out(REG_SCREEN_CTRL, 14);
+	port_byte_out(REG_SCREEN_DATA, (unsigned char)(offset >> 8));
+	port_byte_out(REG_SCREEN_CTRL, 15);
+	port_byte_out(REG_SCREEN_DATA, (unsigned char)(offset & 0xff));
+}
+
+static int get_cursor_offset()
+{
+	port_byte_out(REG_SCREEN_CTRL, 14);
+	int offset = port_byte_in(REG_SCREEN_DATA) << 8;
+	port_byte_out(REG_SCREEN_CTRL, 15);
+	offset += port_byte_in(REG_SCREEN_DATA);
+	return offset * 2;
+}
+
 void clear()
 {
 	int i;
@@ -26,6 +44,8 @@ void clear()
 		screen[i * 2] = ' ';
 		screen[i * 2 + 1] = 0x0f;
 	}
+
+	set_cursor_offset(get_offset(0, 0));
 }
 
 void print()
