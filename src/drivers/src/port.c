ca9495334a09ec02f5ed75ebc1476e55cb4583fc
kithinjibrian <kithinjibrian369@gmail.com>
2024-07-22 20:39:08 +0300

    improved port read and write

--- src/drivers/src/port.c
+++ src/drivers/src/port.c
@@ -1,28 +1,25 @@
 #include "port.h"
 
-/*
-Read a byte from a port.
-*/
 unsigned char port_byte_in(unsigned short port)
 {
 	unsigned char ret;
-	__asm__("in %%dx, %%al" : "=a"(ret) : "d"(port));
+	__asm__ __volatile__("inb %1, %0" : "=a"(ret) : "dN"(port));
 	return ret;
 }
 
 void port_byte_out(unsigned short port, unsigned char data)
 {
-	__asm__ __volatile__("out %%al, %%dx" : : "a"(port), "d"(data));
+	__asm__ __volatile__("outb %0, %1" : : "a"(data), "dN"(port));
 }
 
 unsigned short port_word_in(unsigned short port)
 {
 	unsigned short ret;
-	__asm__("in %%dx, %%ax" : "=a"(ret) : "d"(port));
+	__asm__ __volatile__("inw %1, %0" : "=a"(ret) : "dN"(port));
 	return ret;
 }
 
 void port_word_out(unsigned short port, unsigned short data)
 {
-	__asm__ __volatile__("out %%ax, %%dx" : : "a"(port), "d"(data));
+	__asm__ __volatile__("outw %0, %1" : : "a"(data), "dN"(port));
 }
