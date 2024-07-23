3fea03e25169bbf46b886fc22f3257384d1edfb4
kithinjibrian <kithinjibrian369@gmail.com>
2024-07-23 08:30:43 +0300

    added sti command, somehow the interrupts weren't working without this commad

--- src/cpu/src/idt/i.asm
+++ src/cpu/src/idt/i.asm
@@ -3,6 +3,7 @@ global idt_flush
 idt_flush:
 	mov eax, [esp + 4]
 	lidt [eax]
+	sti
 	ret
 
 %macro ISR_NOERRCODE 1
@@ -22,14 +23,6 @@ idt_flush:
 		jmp isr_common_stub
 %endmacro
 
-%macro IRQ 2
-	global irq%1
-	irq%1:
-		cli
-		push byte 0
-		push byte %2
-		jmp irq_common_stub
-%endmacro
 
 ISR_NOERRCODE 0
 ISR_NOERRCODE 1
@@ -68,23 +61,6 @@ ISR_NOERRCODE 29
 ISR_NOERRCODE 30
 ISR_NOERRCODE 31
 
-IRQ 0, 32
-IRQ 1, 33
-IRQ 2, 34
-IRQ 3, 35
-IRQ 4, 36
-IRQ 5, 37
-IRQ 6, 38
-IRQ 7, 39
-IRQ 8, 40
-IRQ 9, 41
-IRQ 10, 42
-IRQ 11, 43
-IRQ 12, 44
-IRQ 13, 45
-IRQ 14, 46
-IRQ 15, 47
-
 
 extern isr_handler
 isr_common_stub:
@@ -112,6 +88,31 @@ isr_common_stub:
 	sti
 	iret
 
+%macro IRQ 2
+	global irq%1
+	irq%1:
+		cli
+		push byte 0
+		push byte %2
+		jmp irq_common_stub
+%endmacro
+
+IRQ 0, 32
+IRQ 1, 33
+IRQ 2, 34
+IRQ 3, 35
+IRQ 4, 36
+IRQ 5, 37
+IRQ 6, 38
+IRQ 7, 39
+IRQ 8, 40
+IRQ 9, 41
+IRQ 10, 42
+IRQ 11, 43
+IRQ 12, 44
+IRQ 13, 45
+IRQ 14, 46
+IRQ 15, 47
 
 extern irq_handler
 irq_common_stub:
