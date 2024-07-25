b97d7c37e468651b2f995709be780d39cd0ab6f8
Deo-EKodi <deoekodi@gmail.com>
2024-07-25 08:43:14 -0700

    remove duplicate IO

--- src/libc/src/memory.c
+++ src/libc/src/memory.c
@@ -25,3 +25,22 @@ void memset(void *dest, int val, u32_t count)
 	while (count--)
 		*ptr++ = (u8_t)val;
 }
+
+/**
+ * compare values of two memory blocks
+ * @param count compare upto this number of bytes
+ */
+int memcmp(void* s1, void* s2, int count)
+{
+    char* c1 = s1;
+    char* c2 = s2;
+    while(count-- > 0)
+    {
+        if (*c1++ != *c2++)
+        {
+            return c1[-1] < c2[-1] ? -1 : 1;
+        }
+    }
+
+    return 0;
+}
