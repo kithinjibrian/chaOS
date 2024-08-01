0ad3ae763fe382ff2a568247f107fe6c5c6b868e
kithinjibrian <kithinjibrian369@gmail.com>
2024-08-01 18:21:40 +0300

    edits

--- src/libc/include/mstring.h
+++ src/libc/include/mstring.h
@@ -6,6 +6,6 @@
 void htoa(u32_t hex, char *str);
 void itoa(int num, char *str, int base);
 void memset(void *dest, int val, u32_t count);
-void memcpy(char *source, char *dest, u32_t count);
+void memcpy(void *dest, void *source, u32_t count);
 
 #endif
