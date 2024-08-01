25cd7d5c3eaad7d7fcf767fa3a89b64546132594
kithinjibrian <kithinjibrian369@gmail.com>
2024-08-01 18:19:22 +0300

    assert macro to check an expression

---
+++ src/libc/src/assert.c
@@ -0,0 +1,24 @@
+#include "massert.h"
+
+void __assert_fail__(string_t expr, string_t file, u32_t line, string_t func, bool_e panic)
+{
+	if (panic)
+	{
+		print("\nThe kernel panicked!\n");
+	}
+	else
+	{
+		print("\nAssertion failed!\n");
+	}
+
+	print("Expression: %s\n"
+		  "File: %s\n"
+		  "Line: %d\n"
+		  "Function: %s\n",
+		  expr, file, line, func);
+
+	if (panic)
+	{
+		asm volatile("cli \n hlt");
+	}
+}
